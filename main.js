30a6a8d94d343f7398b561aa8f6531fdaa7cbcc1
Saket Roy <saket.roy@hotmail.com>
2020-09-21 13:36:15 +0530

    update json

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const lower_limit = document.getElementById("lower_limit"),
-  upper_limit = document.getElementById("upper_limit"),
-  submit = document.getElementById("submit");
+  upper_limit = document.getElementById("upper_limit");
 
 let upper_limit_val = 100,
   lower_limit_val = 0,
@@ -8,23 +7,31 @@ let upper_limit_val = 100,
   data_quant = [];
 
 $("#submit").click(() => {
-  $("#problems_verbal").empty();
-  $("#problems_quant").empty();
+  $(".row").empty();
+
   data_verbal = [];
   data_quant = [];
 
-  lower_limit_val = isValid(Number(lower_limit.value))
-    ? Number(lower_limit.value)
+  lower_limit_val = isValid(Number($("#lower_limit").val()))
+    ? Number($("#lower_limit").val())
     : 0;
-  upper_limit_val = isValid(Number(upper_limit.value))
-    ? Number(upper_limit.value)
+  upper_limit_val = isValid(Number($("#upper_limit").val()))
+    ? Number($("#upper_limit").val())
     : 100;
 
   filterProblems(lower_limit_val, upper_limit_val);
 });
 
+$("#quant_table_header").click(() => {
+  console.log("fire");
+  data_quant.sort();
+  $(".row").empty();
+
+  createTable();
+});
+
 function filterProblems(ll, ul) {
-  $.getJSON("./assets/data.json", function (json) {
+  $.getJSON("./assets/data_1.json", function (json) {
     let keys = Object.keys(json["verbal"]).map((x) => Number(x));
     for (key in keys) {
       if (key >= ll && key <= ul && json["verbal"][key] !== undefined) {
@@ -45,39 +52,45 @@ function filterProblems(ll, ul) {
 }
 
 function createTable() {
-  $("#problems_verbal").append(
-    `
-    <th>Questions from Verbal Sections:</th>
-    <th>Difficulty (P+):</th>
-    `
-  );
-  data_verbal.forEach((prob) => {
-    prob_info = prob.split("-");
+  if (data_verbal.length === 0) {
     $("#problems_verbal").append(
       `
-      <tr>
+      <tr class="row row_verbal">
+        <td>No Questions in the given range!</td>
+      </tr>
+      `
+    );
+  } else {
+    data_verbal.forEach((prob) => {
+      prob_info = prob.split("-");
+      $("#problems_verbal").append(
+        `
+      <tr class="row row_verbal">
         <td>Test ${prob_info[0]} Section ${prob_info[1]} Q. ${prob_info[2]}</td>
         <td>${prob_info[3]}</td>
       </tr>`
-    );
-  });
-
-  $("#problems_quant").append(
-    `
-    <th>Questions from Quant Sections:</th>
-    <th>Difficulty (P+):</th>
-    `
-  );
-  data_quant.forEach((prob) => {
-    prob_info = prob.split("-");
+      );
+    });
+  }
+  if (data_quant.length === 0) {
     $("#problems_quant").append(
       `
-      <tr>
+      <tr class="row row_quant">
+        <td>No Questions in the given range!</td>
+      </tr>`
+    );
+  } else {
+    data_quant.forEach((prob) => {
+      prob_info = prob.split("-");
+      $("#problems_quant").append(
+        `
+      <tr class="row row_quant">
         <td>Test ${prob_info[0]} Section ${prob_info[1]} Q. ${prob_info[2]}</td>
         <td>${prob_info[3]}</td>
       </tr>`
-    );
-  });
+      );
+    });
+  }
 }
 
 function isValid(num) {
